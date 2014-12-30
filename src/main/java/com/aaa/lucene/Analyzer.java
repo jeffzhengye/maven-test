d732a6dd1df2f44a9a769a023b6725e03cdd479c
jeffzhengye <jeff.zheng.ye@gmail.com>
2014-12-30 01:11:29 -0500

    minor

--- src/main/java/com/aaa/lucene/Analyzer.java
+++ src/main/java/com/aaa/lucene/Analyzer.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cjk.CJKAnalyzer;
 import org.apache.lucene.analysis.cn.ChineseAnalyzer;
+import org.apache.lucene.analysis.cn.ChineseTokenizer;
 import org.apache.lucene.analysis.ngram.NGramTokenizer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
@@ -16,16 +17,17 @@ import org.apache.lucene.util.Version;
 public class Analyzer {
     public static void testStandardTokenStream() throws Exception
     {
-        Reader reader = new StringReader("this is not true, 中国ren人民大会堂, 대단히감사합니다");
+        Reader reader = new StringReader("this China is 123.45 apple-pipe not true, 中国ren人民大会堂, 대단히감사합니다");
         StandardTokenizer gramTokenizer = new StandardTokenizer(Version.LUCENE_47, reader);
         CharTermAttribute charTermAttribute = gramTokenizer.addAttribute(CharTermAttribute.class);
         gramTokenizer.reset();
         while (gramTokenizer.incrementToken()) {
-        String token = charTermAttribute.toString();
-        System.out.println(token);}
+            String token = charTermAttribute.toString();
+            System.out.println(token);
+        }
         gramTokenizer.close();
     }
-    
+
     public static void testNgramTokenStream() throws Exception
     {
         Reader reader = new StringReader("中国ren人民大会堂");
@@ -33,43 +35,52 @@ public class Analyzer {
         CharTermAttribute charTermAttribute = gramTokenizer.addAttribute(CharTermAttribute.class);
         gramTokenizer.reset();
         while (gramTokenizer.incrementToken()) {
-        String token = charTermAttribute.toString();
-        System.out.println(token);}
+            String token = charTermAttribute.toString();
+            System.out.println(token);
+        }
         gramTokenizer.close();
     }
-    
+
     public static void testCJKTokenStream() throws Exception
     {
-      CJKAnalyzer analyzer = new CJKAnalyzer(Version.LUCENE_47);
-      String to_be_parsed = "car window seven中国，人民.";
-      List<String> result = new ArrayList<String>();
-      TokenStream stream  = analyzer.tokenStream("filed", new StringReader(to_be_parsed));
-      stream.reset();
-      while (stream.incrementToken()) {
-          result.add(stream.getAttribute(CharTermAttribute.class).toString());
+        CJKAnalyzer analyzer = new CJKAnalyzer(Version.LUCENE_47);
+        String to_be_parsed = "car window seven中国，人民.";
+        List<String> result = new ArrayList<String>();
+        TokenStream stream = analyzer.tokenStream("filed", new StringReader(to_be_parsed));
+        stream.reset();
+        while (stream.incrementToken()) {
+            result.add(stream.getAttribute(CharTermAttribute.class).toString());
         }
-      System.out.println(result);
-      analyzer.close();
+        System.out.println(result);
+        analyzer.close();
     }
-    
+
     public static void testChineseTokenStream() throws Exception
     {
-      ChineseAnalyzer analyzer = new ChineseAnalyzer();
-      String to_be_parsed = "car window seven中国人民."
-              + "";
-      List<String> result = new ArrayList<String>();
-      TokenStream stream  = analyzer.tokenStream("filed", new StringReader(to_be_parsed));
-      stream.reset();
-      while (stream.incrementToken()) {
-          result.add(stream.getAttribute(CharTermAttribute.class).toString());
+        String to_be_parsed = "car China NSERC window-phone77 14.5 647-70-0716 seven中国88人民99.。"
+                + "";
+        ChineseTokenizer tokenizer = new ChineseTokenizer(new StringReader(to_be_parsed));
+        List<String> result = new ArrayList<String>();
+        CharTermAttribute charTermAttribute = tokenizer.addAttribute(CharTermAttribute.class);
+        tokenizer.reset();
+        while (tokenizer.incrementToken()) {
+            String token = charTermAttribute.toString();
+            System.out.println(token);
         }
-      System.out.println(result);
+        tokenizer.close();
     }
-    
+
+    public static void testCharacter() {
+        // Character.OTHER_LETTER
+        char c = '1';
+        System.out.println(Character.getType(c) == Character.DECIMAL_DIGIT_NUMBER);
+    }
+
     public static void main(String[] args) throws Exception {
-        testStandardTokenStream();
-//        testNgramTokenStream();
-//        testChineseTokenStream();
-//        testCJKTokenStream();
+        // testStandardTokenStream();
+        // testNgramTokenStream();
+         testChineseTokenStream();
+//        testCharacter();
+        // testCJKTokenStream();
     }
 }
