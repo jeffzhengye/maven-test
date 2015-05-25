d6b50e14c2c004db481b39a38a690fb3724bfbff
jeffzhengye <jeff.zheng.ye@gmail.com>
2015-05-25 18:59:02 -0400

    add multi set test

--- src/main/java/com/aaa/guava/SetsTest.java
+++ src/main/java/com/aaa/guava/SetsTest.java
@@ -7,7 +7,9 @@ import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
 
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 
 
@@ -25,11 +27,25 @@ public class SetsTest {
         log.info(diff.toString());
     }
     
+    public static void multiSet() {
+        Multiset<String> bigramCounts = HashMultiset.create();
+        bigramCounts.add("ye");bigramCounts.add("ye");
+        bigramCounts.add("ge"); bigramCounts.add("zde");
+        for(String s : bigramCounts) {
+            System.out.println(s);
+        }
+    }
+    
     /**
      * @param args
      */
     public static void main(String[] args) {
-        testSetOperation();
+//        testSetOperation();
+//        multiSet();
+        System.out.printf("shit%.2f%%\n", 1.2f);
+        System.out.println(String.format("shit%.2f%%", 1.2f));
+        double percent = 34.8;
+        System.out.println(String.format("\t%.2f%%", percent));
     }
 
 }
